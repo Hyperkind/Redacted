b22ee4c57ef821cfcd9151c98053bbfbbfa67664
Jonathan Ah-Nee <jonahnee@gmail.com>
2016-01-06 21:51:04 -1000

    got post req to work

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var jade = require('jade');
 
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', './views');
+
 var replacements = [
   {'selfie': 'self-portrait'},
   {'yummers': 'delicious'},
@@ -14,17 +19,21 @@ var replacements = [
   {'yolo': 'carpe diem'}
 ];
 
-var app = express();
+// parses the URL-encoded data with the querystring library
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.set('view engine', 'jade');
-app.set('views', './views');
+var sentence = [];
 
 app.get('/', function (req, res) {
   res.render('index', {message: 'testing this out', pageTitle: 'Render'});
+  console.log('getting index page');
 });
 
 app.post('/', function (req, res) {
-
+  var sent = req.body.message;
+  sentence.push(sent);
+  res.send('Sent the message: ' + sent);
+  console.log(sent);
 });
 
 var server = app.listen(8080, function() {

4ef36aacc9a1a0acad181e04365e6eda5bd1ab95
Jonathan Ah-Nee <jonahnee@gmail.com>
2016-01-06 21:28:01 -1000

    got jade to render, added replacement words object

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var jade = require('jade');
+
+var replacements = [
+  {'selfie': 'self-portrait'},
+  {'yummers': 'delicious'},
+  {'outchhea': 'are out here'},
+  {'bruh': 'wow'},
+  {'doge': 'pug'},
+  {'cilantro': 'soap'},
+  {'bae': 'loved one'},
+  {'swag': 'style'},
+  {'yolo': 'carpe diem'}
+];
 
 var app = express();
 
+app.set('view engine', 'jade');
+app.set('views', './views');
+
 app.get('/', function (req, res) {
-  res.send('yes this works');
+  res.render('index', {message: 'testing this out', pageTitle: 'Render'});
+});
+
+app.post('/', function (req, res) {
+
 });
 
 var server = app.listen(8080, function() {

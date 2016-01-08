7ba06e5447314658ce3229fd895157ac2570f507
Jonathan Ah-Nee <jonahnee@gmail.com>
2016-01-07 19:59:39 -1000

    got it translating

--- app.js
+++ app.js
@@ -7,33 +7,56 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', './views');
 
-var replacements = [
-  {'selfie': 'self-portrait'},
-  {'yummers': 'delicious'},
-  {'outchhea': 'are out here'},
-  {'bruh': 'wow'},
-  {'doge': 'pug'},
-  {'cilantro': 'soap'},
-  {'bae': 'loved one'},
-  {'swag': 'style'},
-  {'yolo': 'carpe diem'}
-];
+var replacements = {
+  selfie: 'self-portrait',
+  yummers: 'delicious',
+  outchhea: 'are out here',
+  bruh: 'wow',
+  doge: 'pug',
+  cilantro: 'soap',
+  bae: 'loved one',
+  swag: 'style',
+  yolo: 'carpe diem'
+};
 
 // parses the URL-encoded data with the querystring library
 app.use(bodyParser.urlencoded({extended: false}));
 
-var sentence = [];
+var orig;
+
+app.use('/', fixSentence);
+
+function fixSentence (req, res, next) {
+  var clean = [];
+  var cleanWord;
+  var origSplit = req.body.message.split(" ");
+  // origSplit = res.locals.message = origSplit.split(" ");
+  console.log(origSplit);
+  for (var i = 0; i < origSplit.length; i++) {
+    var currentWord = origSplit[i];
+    if (Object.keys(replacements).indexOf(currentWord) > -1) {
+      cleanWord = replacements[currentWord];
+      clean.push(cleanWord);
+    } else {
+      clean.push(currentWord);
+    }
+  }
+  var cleanSent = clean.join(' ');
+  console.log(cleanSent);
+  return next();
+}
 
 app.get('/', function (req, res) {
-  res.render('index', {message: 'testing this out', pageTitle: 'Render'});
+  res.render('index', {header: 'Please input your message:' , submit: 'Submit', pageTitle: 'Redacted'});
   console.log('getting index page');
 });
 
 app.post('/', function (req, res) {
-  var sent = req.body.message;
-  sentence.push(sent);
-  res.send('Sent the message: ' + sent);
-  console.log(sent);
+  orig = req.body.message;
+  // sentence.push(sent);
+  res.send('Sent the message: ' + orig);
+  console.log('original message: ' + orig);
+  // console.log(sentence);
 });
 
 var server = app.listen(8080, function() {
